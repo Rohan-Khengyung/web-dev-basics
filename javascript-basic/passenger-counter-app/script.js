5d7ae2b60f353a3bf5fc5dea9b3640b50499834f
Rohan Khengyung <kazkozan38@gmail.com>
2025-11-08 17:58:56 +0545

    reer

--- javascript-basic/passenger-counter-app/script.js
+++ javascript-basic/passenger-counter-app/script.js
@@ -1,22 +1,11 @@
-// intialize the count as 0 
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
-
-// document.getElementById("count-el").innerText = 5
-
-// camelCase
-
 let countEl = document.getElementById("count-el")
-
-console.log(countEl)
-
 let count = 0
 
 function increment() {
-    console.log("clicked")
     count += 1
     countEl.innerText = count
-    console.log(count)
 }
 
+function save(){
+    console.log(count)
+}
