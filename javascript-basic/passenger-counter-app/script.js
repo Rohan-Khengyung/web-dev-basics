df476d5fd24f1576534373749e1bccd5d185bc76
Rohan Khengyung <kazkozan38@gmail.com>
2025-11-08 07:54:49 +0545

    commit-fe

--- javascript-basic/passenger-counter-app/script.js
+++ javascript-basic/passenger-counter-app/script.js
@@ -12,3 +12,20 @@
 // console.log(myDogAge)
 
 
+// let bonusPoints = 50
+
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 50
+
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+
+// console.log(bonusPoints)
+
+
