f44e818db80453613e95b8d9b5f9217450bf1c7c
Rohan Khengyung <kazkozan38@gmail.com>
2025-11-09 06:23:40 +0545

    jsldf-

--- javascript-basic/passenger-counter-app/script.js
+++ javascript-basic/passenger-counter-app/script.js
@@ -1,11 +1,12 @@
-let countEl = document.getElementById("count-el")
-let count = 0
+// let countEl = document.getElementById("count-el")
+// let count = 0
 
-function increment() {
-    count += 1
-    countEl.innerText = count
-}
+// function increment() {
+//     count += 1
+//     countEl.innerText = count
+// }
 
-function save(){
-    console.log(count)
-}
+// function save(){
+//     console.log(count)
+// }
+
