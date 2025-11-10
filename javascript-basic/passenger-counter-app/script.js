82f853e68d84cddb54ae944edf7713fd335f9270
Rohan Khengyung <kazkozan38@gmail.com>
2025-11-10 18:19:37 +0545

    counter-app

--- javascript-basic/passenger-counter-app/script.js
+++ javascript-basic/passenger-counter-app/script.js
@@ -1,14 +1,17 @@
-// let countEl = document.getElementById("count-el")
-// let count = 0
+let countEl = document.getElementById("count-el")
+let count = 0
 
-// function increment() {
-//     count += 1
-//     countEl.innerText = count
-// }
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
 
-// function save(){
-//     console.log(count)
-// }
 
-let wish = "I'd love to see the show \"The Lioi King\"."
-console.log(wish)
+let saveEl = document.getElementById("save-el")
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
