5547e28b4de0a3087d83fdd5bc19974cc35a81c0
Rohan Khengyung <kazkozan38@gmail.com>
2025-11-11 10:09:31 +0545

    p-js

---
+++ javascript-basic/variables-practice/script.js
@@ -0,0 +1,39 @@
+//Variables
+// let firstName = "Rohan"
+// let lastName = "Rai"
+// let fullName = firstName + " " + lastName
+// console.log(fullName)
+
+//Function
+// let name = "Linda"
+// let greeting = "Hi there"
+// function greetLinda(){
+//     console.log(greeting + ", " + name + "!")
+// }
+// greetLinda()
+
+//Icrement and Decrement
+// let myPoints = 3
+// function add3Points(){
+//     myPoints += 3
+// }
+// function remove1Point(){
+//     myPoints -= 1
+// }
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+// console.log(myPoints)
+
+
+//Strings and Integer
+// console.log("2" + 2) // "22"
+// console.log(11 + 7) // 18
+// console.log("6" + 5) // "65"
+// console.log("My poinst: " + 5 + 9) // "My points: 59"
+// console.log(2 + 2) // 4
+// console.log("11" + "14") // "1114"
+
+
