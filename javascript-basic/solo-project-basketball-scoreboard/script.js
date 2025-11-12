84948c5fc68a1479b597471a70e486dfb38ac8e5
Rohan Khengyung <kazkozan38@gmail.com>
2025-11-12 11:11:07 +0545

    score-c

--- javascript-basic/solo-project-basketball-scoreboard/script.js
+++ javascript-basic/solo-project-basketball-scoreboard/script.js
@@ -1,1 +1,51 @@
-//
+
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
+//Home function
+let addOne = 0
+let addTwo = 0
+let addThree = 0
+function plusOne(){
+    addOne += 1
+    let scoreOne = addOne + addTwo + addThree
+    homeScore.textContent = scoreOne
+}
+function plusTwo(){
+    addTwo += 2
+    let scoreTwo = addOne + addTwo + addThree
+    homeScore.textContent = scoreTwo
+}
+function plusThree(){
+    addThree += 3
+    let scoreThree = addOne + addTwo + addThree
+    homeScore.textContent = scoreThree
+}
+
+
+
+//Guest functions
+let addOnes = 0
+let addTwos = 0
+let addThrees = 0
+function plusOnes(){
+    addOnes += 1
+    let scoreOnes = addOnes + addTwos + addThrees
+    guestScore.textContent = scoreOnes
+}
+function plusTwos(){
+    addTwos += 2
+    let scoreTows = addOnes + addTwos + addThrees
+    guestScore.textContent = scoreTows
+}
+function plusThrees(){
+    addThrees += 3
+    let scoreThrees = addOnes + addTwos + addThrees
+    guestScore.textContent = scoreThrees
+}
+
+//Next Game
+function nextGame(){
+    guestScore.textContent = 0
+    homeScore.textContent = 0
+}
