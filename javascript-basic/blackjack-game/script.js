da9e5eeae91d4e44abe27bec6e17f981a9659fbf
Rohan Khengyung <kazkozan38@gmail.com>
2025-11-14 17:36:31 +0545

    js-commit

--- javascript-basic/blackjack-game/script.js
+++ javascript-basic/blackjack-game/script.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "Per",
+    chips: 200
+}
 
 let cards = []
 let sum = 0
@@ -7,27 +11,23 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
-    let randomNumer = Math.floor( Math.random()*13 ) + 1
-    if (randomNumer === 1) {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
         return 10
-    } else if (randomNumer > 1) {
+    } else if (randomNumber === 1) {
         return 11
     } else {
-        return randomNumer
+        return randomNumber
     }
 }
 
-let player = {
-    name: "Per",
-    chips: 145
-}
-
-let playerEl = document.getElementById("player-el")
-playerEl.textContent = player.name + ": $" + player.chips
-
 function startGame() {
+    isAlive = true
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
     cards = [firstCard, secondCard]
@@ -37,11 +37,12 @@ function startGame() {
 
 function renderGame() {
     cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++){
+    for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
+    
     sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) { 
+    if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
         message = "You've got Blackjack!"
@@ -55,13 +56,10 @@ function renderGame() {
 
 
 function newCard() {
-    if (isAlive === true && hasBlackJack === false){
-        drawNewCard()
-    }
-    function drawNewCard(){
+    if (isAlive === true && hasBlackJack === false) {
         let card = getRandomCard()
         sum += card
         cards.push(card)
-        renderGame()
+        renderGame()        
     }
 }
