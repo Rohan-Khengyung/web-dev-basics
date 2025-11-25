19d90960a793324d4c786ca60a645c5a44e07c73
Rohan Khengyung <kazkozan38@gmail.com>
2025-11-25 19:37:09 +0545

    for

--- javascript-basic/unit-converter/script.js
+++ javascript-basic/unit-converter/script.js
@@ -1,3 +1,9 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
 const convBtn = document.getElementById("convert-btn")
 
 convBtn.addEventListener("click", function(){
