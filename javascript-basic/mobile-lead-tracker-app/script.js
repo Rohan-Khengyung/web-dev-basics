476e542b3a4b76cf85f7aebc5a85125006191d49
Rohan Khengyung <kazkozan38@gmail.com>
2025-11-26 09:51:00 +0545

    mobile-app

--- javascript-basic/mobile-lead-tracker-app/script.js
+++ javascript-basic/mobile-lead-tracker-app/script.js
@@ -1,23 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js"
+ 
+ const firebaseConfig = {
+    databaseURL: "https://leads-tracker-app-181ea-default-rtdb.asia-southeast1.firebasedatabase.app/"
+ }
+ 
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
 
 function render(leads) {
     let listItems = ""
